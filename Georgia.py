1fcafc6f40117c26a1a39bde13b9dc063fae930d
Napervillpol <110348077+Napervillpol@users.noreply.github.com>
2022-08-12 19:01:59 -0500

    add GeoJson

--- Georgia.py
+++ Georgia.py
@@ -40,8 +40,6 @@ def get_candidate(name, xpath):
     return df
 
 
-print('Download Starting...')
-
 url = 'https://results.enr.clarityelections.com//GA//107556/275242/reports/detailxml.zip'
 
 r = requests.get(url)
@@ -50,8 +48,6 @@ filename = url.split('/')[-1]  # this will take only -1 splitted part of the url
 
 with open(filename, 'wb') as output_file:
     output_file.write(r.content)
-
-print('Download Completed!!!')
 
 zf = ZipFile('detailxml.zip', 'r')
 zf.extractall()
@@ -65,43 +61,8 @@ df1 = get_candidate('Perdue', ".//Choice[@key='4']")
 
 df = df.merge(df1, on='Counties')
 
-#df.insert(3, "Margin", safediv(df['Levin'] - df['Haley'], df['Levin'] + df['Haley']))
-
 df.to_csv('Output.csv', index=False)
+df.to_json('runoff.json')
 
-file_name='OC_Voting_Precinct.geojson'
-
-
-with open(file_name, 'r', encoding='utf-8') as f:
-    data = json.load(f)
-
-#with urlopen("https://gisservices.oakgov.com/arcgis/rest/services/Enterprise/EnterpriseAdminDataMapService/MapServer/7/query?outFields=*&where=1%3D1&f=geojson") as response:
-#    source = response.read()
-
-#data = json.loads(source)
-
-i = 0
-for features in data['features']:
-
-    features['properties']['Levin'] = 0
-    features['properties']['Haley'] = 0
-    features['properties']['Margin']= 0
-    for Precincts in df['Precinct']:
-
-        if features['properties']['PrecinctName'] == Precincts:
-            features['properties']['Levin'] = int(df['Levin'][i])
-            features['properties']['Haley'] = int(df['Haley'][i])
-            features['properties']['Margin'] = float(df['Margin'][i])
-            #print(features['properties']['PrecinctName'])
-        i = i+1
-    i=0
-
-
-
-#json_data = json.dumps(data)
-
-with open('new_file.geojson', 'w') as f:
-    json.dump(data, f, indent=2)
-    print("The json file is created")
 
 
